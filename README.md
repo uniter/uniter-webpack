5f706639a056eab818f2b42552a8b3f28b65b00a
Daniel Phillimore <dan@ovms.co>
2015-05-18 13:06:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Uniter-Webpack
+==============
+
+A demo of bundling a Uniter-based app using Webpack.
+
+To compile and run:
+
+```javascript
+npm run build
+```
+
+and then open `index.html` in a browser.
---
+++ app/index.js
@@ -0,0 +1,30 @@
+/*
+ * Demo of bundling a Uniter app with Webpack.
+ *
+ * MIT license.
+ */
+'use strict';
+
+var uniter = require('uniter/js/main'),
+    phpEngine = uniter.createEngine('PHP'),
+    output = document.getElementById('output');
+
+// Set up a PHP module loader
+phpEngine.configure({
+    include: function (path, promise) {
+        // Wait 500ms before responding with the file, just to demonstrate async support
+        setTimeout(function () {
+            promise.resolve('<?php return "Hello from ' + path + '!";');
+        }, 500);
+    }
+});
+
+// Print anything written to stdout to the console
+phpEngine.getStdout().on('data', function (data) {
+    output.insertAdjacentHTML('beforeEnd', '<p>' + data + '</p>');
+});
+
+// Go!
+phpEngine.execute('<?php print "Required: " . require("test.php") . "!";').done(function () {
+    output.insertAdjacentHTML('beforeEnd', '<p>Done - the 500ms delay was intentional, by the way!</p>');
+});
